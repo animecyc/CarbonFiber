f283bdc6c160214a988c2ce8b505196c9027b9d6
Seth Benjamin <animecyc@gmail.com>
2014-01-10 17:16:31 -0500

    Added support for animating classes.

--- assets/lib/carbon.animate.js
+++ assets/lib/carbon.animate.js
@@ -6,6 +6,7 @@
 	 */
 	var CarbonAnimation = function () {
 		this.animator = require('com.animecyc.animator');
+		this.propertyProxy = require('lib/carbonfiber/util.propertyProxy');
 	};
 
 	/**
@@ -25,6 +26,56 @@
 				}
 			});
 		}
+	};
+
+	/**
+	 * Animate a class addition
+	 *
+	 * @param {Controller} controller The Alloy controller to pull styles form
+	 * @param {Ti.UI.View} view       The view to apply the class animation
+	 * @param {Object}     properties A dictionary of properties to animate
+	 * @param {Function}   callback   Function to call when animation completes
+	 */
+	CarbonAnimation.prototype.addClassAnimated = function (controller, view, properties, callback) {
+		var self = this,
+			properties = new this.propertyProxy(view);
+
+		controller.addClass(properties, properties.classes.split(' '));
+
+		this.animate(
+			view,
+			_.extend(_.omit(properties, 'classes'), properties.finalize()),
+			function () {
+				if (_.isFunction(callback)) {
+					callback.call(self);
+				}
+			}
+		);
+	};
+
+	/**
+	 * Animate a class removal
+	 *
+	 * @param {Controller} controller The Alloy controller to pull styles form
+	 * @param {Ti.UI.View} view       The view to apply the class animation
+	 * @param {Object}     properties A dictionary of properties to animate
+	 * @param {Function}   callback   Function to call when animation completes
+	 */
+	CarbonAnimation.prototype.removeClassAnimated = function (controller, view, properties, callback) {
+		var self = this,
+			properties = new this.propertyProxy(view);
+
+		controller.removeClass(properties, properties.classes.split(' '));
+
+		this.animate(
+			view,
+			_.extend(_.omit(properties, 'classes'), properties.finalize()),
+			function () {
+				if (_.isFunction(callback)) {
+					callback.call(self);
+				}
+			}
+		);
 	};
 
 	exports = module.exports = new CarbonAnimation();
---
+++ assets/lib/util.propertyProxy.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    /**
+     * A proxy for routing properties
+     *
+     * @param {Ti.UI.View} view The view to assert properties against
+     */
+    var PropertyProxy = function (view) {
+    	if (_.isObject(view)) {
+    		this.__view = view;
+
+            _.extend(this, view);
+    	}
+    	else {
+    		throw 'Invalid proxy parameters';
+    	}
+    };
+
+    /**
+     * Apply properties to the prototype
+     */
+    PropertyProxy.prototype.applyProperties = function () {
+    	if (_.isObject(arguments[0])) {
+    		_.extend(this, arguments[0]);
+    	}
+    	else {
+    		throw 'Properties must be an object';
+    	}
+    };
+
+    /**
+     * Applies the set classes to the
+     * supplied Ti.UI.View and returns
+     * the computed properties
+     *
+     * @return {Object} The computed properties
+     */
+    PropertyProxy.prototype.finalize = function () {
+    	this.__view.classes = this.classes;
+
+        return _.omit(this, 'applyProperties', 'finalize', '__view')
+    };
+
+    exports = module.exports = PropertyProxy;
+}).call(this);
