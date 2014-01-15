b8d3f165d35ae2b67d2258626d3e25b898c0b7a6
Seth Benjamin <animecyc@gmail.com>
2014-01-15 15:33:13 -0500

    Fixing some jshint errors.

--- assets/lib/carbon.colors.js
+++ assets/lib/carbon.colors.js
@@ -6,22 +6,22 @@
      */
 	var Colors = function () {
         this.colors = {
-            aqua : "#00ffff",
-            black : "#000000",
-            blue : "#0000ff",
-            fuchsia : "#ff00ff",
-            gray : "#808080",
-            green : "#008000",
-            lime : "#00ff00",
-            maroon : "#800000",
-            navy : "#000080",
-            olive : "#808000",
-            purple : "#800080",
-            red : "#ff0000",
-            silver : "#c0c0c0",
-            teal : "#008080",
-            white : "#ffffff",
-            yellow : "#ffff00"
+            aqua : '#00ffff',
+            black : '#000000',
+            blue : '#0000ff',
+            fuchsia : '#ff00ff',
+            gray : '#808080',
+            green : '#008000',
+            lime : '#00ff00',
+            maroon : '#800000',
+            navy : '#000080',
+            olive : '#808000',
+            purple : '#800080',
+            red : '#ff0000',
+            silver : '#c0c0c0',
+            teal : '#008080',
+            white : '#ffffff',
+            yellow : '#ffff00'
         };
 	};
 
